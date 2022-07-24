f840074fa5e13791cb0ec4037c4997114045de9d
saeedeh-asgharpour <saeedeh.asqarpour@gmail.com>
2022-07-24 22:42:13 +0430

    Configure SQL server

--- tickets/Data/AppDbContext.cs
+++ tickets/Data/AppDbContext.cs
@@ -11,12 +11,25 @@ namespace tickets.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Actor_Movie>().HasKey(am => new{
-            
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
 
-            })
-                base.OnModelCreating(modelBuilder);
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m =>
+            m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m =>
+           m.ActorId);
+
+            base.OnModelCreating(modelBuilder);
         }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
 
     }
 }
--- tickets/Program.cs
+++ tickets/Program.cs
@@ -1,11 +1,15 @@
 
+using Microsoft.EntityFrameworkCore;
 using tickets.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>();
+
+//DbContext Configuration
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString
+    ("DefaultConnrctionString")));
 
 
 var app = builder.Build();
